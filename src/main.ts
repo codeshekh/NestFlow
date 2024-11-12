192b2eed5112eec1e6cc2c7945723af19d5b37c7
codeshekh <pandeyabhishek1518@gmail.com>
2024-11-12 10:38:13 +0530

    adding PORT

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,25 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
+// Load environment variables
+dotenv.config();
+
+// Set the port, with a fallback to 3000
+const PORT = process.env.PORT || 5000;
+
 async function bootstrap() {
-  
-  dotenv.config();
-
-
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS with a wildcard origin
   const corsOptions: CorsOptions = {
     origin: '*', 
   };
   app.enableCors(corsOptions);
 
+  // Set a global prefix for all routes
   app.setGlobalPrefix('api/v1');  
 
-
+  // Configure Swagger for API documentation
   const config = new DocumentBuilder()
     .setTitle('Task Generator')
     .setDescription('API Documentation for Task Generator')
@@ -26,12 +31,12 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  
-  SwaggerModule.setup('api/v1/docs', app, document);  
+  SwaggerModule.setup('api/v1/docs', app, document);
 
-
-  await app.listen(5000);
-  console.log(`Application is running on: ${await app.getUrl()}/api/v1/docs`); 
+  // Start the application and log the URL
+  await app.listen(PORT);
+  console.log(`Server is listening on port: ${PORT}`);
+  console.log(`API documentation is available at: ${await app.getUrl()}/api/v1/docs`); 
 }
 
 bootstrap();
